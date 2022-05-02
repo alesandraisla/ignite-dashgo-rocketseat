257ad6f05697039f2fc5af5919091fba1a13a026
AlesandraIsla <alesandramempis@gmail.com>
2022-05-01 21:23:40 -0300

    MirageJs e reactQuery

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,11 +1,22 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import Link from 'next/link';
+import { useEffect } from "react";
+import {useQuery } from 'react-query';
 
 export default function UserList() {
+    //valores de dentro do query 
+    const { data, isLoading, error } = useQuery('users', async () => {
+        const response = await fetch('http://localhost:3000/api/users')
+        const data = response.json()
+        
+        return data;
+    })
+
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
@@ -37,35 +48,48 @@ export default function UserList() {
                             </Button>
                         </Link>
                     </Flex>
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>Usuário</Th>
-                                { isWideVersion && <Th>Data de cadastro</Th>}
-                                <Th width={8}></Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Alesandra Marinho</Text>
-                                        <Text fontSize="sm" color="gray.300">alesandramempis@hmail.com</Text>
-                                    </Box>
-                                </Td>
-                                { isWideVersion && <Td>
-                                    04 de Abril, 2022
-                                </Td>}
-                            </Tr>
-                        </Tbody>
-                    </Table>
-                    <Pagination />
+                    { isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>Falha ao obter dados do usuário</Text>
+
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table colorScheme="whiteAlpha">
+                                <Thead>
+                                    <Tr>
+                                        <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                                            <Checkbox colorScheme="pink" />
+                                        </Th>
+                                        <Th>Usuário</Th>
+                                        { isWideVersion && <Th>Data de cadastro</Th>}
+                                        <Th width={8}></Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    <Tr>
+                                        <Td px={["4", "4", "6"]}>
+                                            <Checkbox colorScheme="pink" />
+                                        </Td>
+                                        <Td>
+                                            <Box>
+                                                <Text fontWeight="bold">Alesandra Marinho</Text>
+                                                <Text fontSize="sm" color="gray.300">alesandramempis@hmail.com</Text>
+                                            </Box>
+                                        </Td>
+                                        { isWideVersion && <Td>
+                                            04 de Abril, 2022
+                                        </Td>}
+                                    </Tr>
+                                </Tbody>
+                            </Table>
+                            <Pagination />
+                        </>
+                    )}
                 </Box>
             </Flex>
         </Box>
